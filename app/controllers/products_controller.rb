7297917cda8fbfccb356fe20277db95a6c46af5a
Bryan Lin <bryan@epicodus.com>
2017-12-11 16:51:15 -0800

    add new product functionality in controller and views, and edit product validation for rating

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,4 +4,22 @@ class ProductsController < ApplicationController
     @products = Product.all
   end
 
+  def new
+    @product = Product.new
+  end
+
+  def create
+    if product = Product.create!(product_params)
+      redirect_to products_path, notice: "Product successfully added."
+    else
+      render :new
+    end
+  end
+
+private
+
+  def product_params
+    params.require(:product).permit(:name, :description, :price, :image)
+  end
+
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,8 @@
 class Product < ApplicationRecord
 
-  validates :rating, numericality: { only_integer: true, greater_than: 0, less_than_or_equal_to: 5 }
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
+  validates :rating, numericality: { only_integer: true, greater_than: 0, less_than_or_equal_to: 5 }, allow_nil: true
 
 end
