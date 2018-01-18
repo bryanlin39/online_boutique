c01ac105cee0ab77e156d634813eb0b5a6e21718
Bryan Lin <bryanlin39@gmail.com>
2018-01-18 13:02:46 -0800

    add s3_host_name configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,6 +97,7 @@ Rails.application.configure do
       secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
       s3_region: ENV.fetch('AWS_REGION'),
     },
-    s3_protocol: 'https'
+    s3_protocol: 'https',
+    s3_host_name: "s3-#{ENV['AWS_REGION']}.amazonaws.com"
   }
 end
